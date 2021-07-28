d30bbe347d5499f5ab20cee20bc9bedf362bd666
Sarbjit Singh <sarbjeets18@gmail.com>
2021-07-28 11:12:54 +0100

    new environment variables added

--- startup/db.js
+++ startup/db.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose')
 const winston = require('winston')
 const config = require('config')
+const username = config.get('user') //username of remote mongodb 
+const password = config.get('pass') // password of remote mongodb
+const dbName = config.get('dbName')
+
+const remote_url = `mongodb+srv://${username}:${password}@cluster0.suij1.mongodb.net/${dbName}?retryWrites=true&w=majority`
 let db_url = ""
 const db = () => {
 
-    if (process.env.NODE_ENV == 'production')
-        db_url = config.get('remote_db_url')
-    else {
-
-        // console.log(process.env.NODE_ENV)
-        db_url = config.get('local_db_url')
-    }
+    if (process.env.NODE_ENV == 'production') //code deploy on heroku server
+        db_url = remote_url //connect with cloud mongodb server 
+    else
+        db_url = config.get('local_db_url') + dbName //connect with local mongodb server
 
     mongoose.connect(db_url, {
             useNewUrlParser: true,
